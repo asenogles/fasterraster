50141107c66ed6314286705cc66b5eb673bbe61f
asenogles <senoglea@oregonstate.edu>
2023-03-08 15:14:04 -0800

    import compute_epe

--- fasterraster/__init__.py
+++ fasterraster/__init__.py
@@ -1,6 +1,6 @@
 from .bil.bil import Bil
 from .bsq.bsq import Bsq
 from .tif.tif import Tif
-from .flo.flo import Flo
+from .flo.flo import Flo, compute_epe
 from .raster import Raster, read, convolve2d, inpaint_nans
 from ._ext import *
